f5b1026635e058f2740af338bd66260966c9369e
Allan Gamurot <allandaveghwb@gmail.com>
2023-08-24 23:15:20 +0800

    edit heading text

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -28,9 +28,10 @@ const TodoWrapper = () => {
   const editTask = (task, id) => {
     setTodos(todos.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo))
   }
+  
   return (
     <div className='bg-[#CEDEBD] w-[400px] mx-auto mt-[100px] rounded-md'>
-        <h1 className='text-center text-[#435334] text-2xl font-semibold pt-2'>Get Things Done!</h1>
+        <h1 className='text-center text-[#435334] text-2xl font-semibold pt-2'>Get Things Done, Pangit!</h1>
         <div className='pb-2'>
           <TodoForm addTodo={addTodo}/>
           <div className='pt-2'>
